fe349db8368b0dbb4d07909ad106f64f1401369f
sdiakhate <liousadiakhate20@gmail.com>
2023-10-12 23:11:13 +0200

    filter

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import "../assets/styles/card.css";
 
 function Card(props) {
-  console.log(props.category);
+  // console.log(props.category);
   return (
     <div className="card">
       <img src={props.image} alt={props.title} />
