0b9dac35d47d2a7e7c8f66de1ebbb41184142573
sdiakhate <liousadiakhate20@gmail.com>
2023-10-12 14:07:12 +0200

    fix conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import data from "./data/data.json";
 import Navbar from "./components/Navbar";
 
 function App() {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import "../assets/styles/navBar.css";
 
 function Navbar() {
   return (
-    <header className="d-flex align-items-center">
+    <header className="d-flex align-items-center p-5">
       <img src="src/assets/images/logo.jpg" alt="logo" />
       <p>Atlas Home</p>
     </header>
